29e152747e5bfbf5db3980629c4f475e06f30771
LeeSeungCheon <koogk7@gmail.com>
2019-10-24 16:58:34 +0900

    영상 전환시 동작 설정
    # Todo
    1. 페이지 전환시 확장앱 자막 더미들 초기화

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import Subtitle from './entity/Subtitle.js';
 import DownloadAdmin from './service/download.js';
 
 class ReadVideo {
+    /* Todo
+    resetSidePage - 사이드바가 열렸을 때, 확장앱 페이지를 비움
+    
+     */
 
     constructor() {
         console.log("Success Load");
@@ -13,9 +17,8 @@ class ReadVideo {
         this.track= document.querySelector('track');
         this.downloadAdmin = new DownloadAdmin();
 
-        this.initExecuteCode();
-        console.log(this.donwloadBtn);
         this.donwloadBtn.addEventListener('click', this.downloadAdmin.test);
+        this.whaleEventListener();
 
         console.log("Success Init");
     }
@@ -25,19 +28,31 @@ class ReadVideo {
         whale.tabs.executeScript({
             code: `
         window.showSubtitle = new ${ShowSubtitle}();
+      `
+        });// 왜 전역으로 안되지 ==> 페이지가 새로 로드되니까 애가 날라가지...
+    }
+
+    loadSubtitles(){
+        whale.tabs.executeScript({
+            code: `
+        console.log(window.showSubtitle);
         window.showSubtitle.getSubtitles();
       `
-        }, function (result) { //  실행된 코드 마지막 결과를 받아온다.
+        }, (result) => { //  실행된 코드 마지막 결과를 받아온다.
             //Todo null 처리
-
             if(result == null)
                 console.log("자막을 가져오는데 오류가 발생했습니다.");
-            else
+            else{
+                console.log('자막 추출 성공');
+                console.log(result);
                 this.subtitles = this.transSubtitles(result[0]);
+                console.log(this.subtitles);
+                // Todo 확장앱에 데이터 삽입
                 this.renderSubtitle(this.subtitles);
-            // Todo 확장앱에 데이터 삽입
-        }.bind(this));
-    };
+            }
+
+        });
+    }
 
     transSubtitles(subtitlesText) {
         let paragraphSplit = subtitlesText.split('\n\n');
@@ -50,18 +65,16 @@ class ReadVideo {
                 rst.push(subtitle);
         });
 
-        console.log("=============");
-        console.log(rst);
         return rst;
     }
 
     renderSubtitle(subtitles){
 
-        let contentNode = document.querySelector('.subtitle_wrap');
+        let subtitleList = document.querySelector('.subtitle_list');
         console.log("=====RenderSubtitle====");
 
         subtitles.map(item => {
-            let subtitleDiv = document.createElement('div');
+            let subtitleWrap = document.createElement('div');
             let progressBar = document.createElement('span');
             let subtitleContent = document.createElement('span');
 
@@ -69,15 +82,48 @@ class ReadVideo {
             /*
             Todo 노드 속성 등을 정해줘야함
              */
-            progressBar.textContent = '|';
+            subtitleWrap.classList.add('subtitle_wrap');
+            progressBar.classList.add('progress_bar');
+            subtitleContent.classList.add('subtitle_content');
+
             subtitleContent.textContent = item.content;
 
-            subtitleDiv.appendChild(progressBar);
-            subtitleDiv.appendChild(subtitleContent);
-            contentNode.appendChild(subtitleDiv);
+
+            subtitleWrap.appendChild(progressBar);
+            subtitleWrap.appendChild(subtitleContent);
+            subtitleList.appendChild(subtitleWrap);
         })
 
     }
+
+    whaleEventListener(){
+
+        // 탭이 업데이트 되었을때, 다시 문서에서 단어를 검색하도록
+        whale.tabs.onUpdated.addListener((id, changeInfo) => {
+            if(changeInfo.status === 'complete') {
+                this.initExecuteCode();
+            }
+        });
+
+        // 다른 탭이 활성화 되었을때
+        whale.tabs.onActivated.addListener(() => {
+        });
+
+        // 탭이 종료되었을때
+        whale.tabs.onRemoved.addListener((id) => {
+
+        });
+
+        // 사이드바가 활성화 되었을때
+        document.addEventListener('visibilitychange', ()=>{
+            if (document.visibilityState === `visible`) {
+                // 사이드바가 열렸을 때
+                console.log('Open Sidebar');
+                this.loadSubtitles();
+            }
+        }); // 화살표함수는 자체적으로 this를 bind 하지 않음
+    }
+
 
 }
 
--- src/service/showSubtitle.js
+++ src/service/showSubtitle.js
@@ -1,52 +1,51 @@
 export default class ShowSubtitle { // Todo 이름 마음에 안듬
-  constructor() {
-    this.body = document.body;
-  }
+    constructor() {
+        this.body = document.body;
+    }
 
-  getSubtitles() {
+    getSubtitles() {
+        let track = document.querySelector('track');
+        let trackAddress = track == null ? '' : track.getAttribute('src');
 
-      let track = document.querySelector('track');
-      let trackAddress = track == null ? '' : track.getAttribute('src');
-
-      if (track == null) {
-          console.log("페이지에서 자막을 찾을 수 없습니다.");
-          return null;
-      }
-      console.log("자막 추출 성공 : " + track.getAttribute('src'));
-
-      // 비동기로 자막 내용을 가져와야함 ==> URL scheme must be "http" or "https" for CORS request. 에러 발생
-      // let test = await fetch(trackAddress)
-      //     .then(res => res.json())
-      //     .then(res => res);
-
-      let subtitle = this.requestSutitle(trackAddress);
-      console.log(subtitle);
-
-      return subtitle;
-  }
-
-requestSutitle(address) {
-    const xhr = new XMLHttpRequest();
-    let subtitle = '';
-
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState !== XMLHttpRequest.DONE) return;
-
-        if (xhr.status === 200) {
-            document.getElementById('content').innerHTML = xhr.responseText;
-            subtitle = xhr.responseText;
-        } else {
-            console.log(`[${xhr.status}] : ${xhr.statusText}`);
+        if (track == null) {
+            console.log("페이지에서 자막을 찾을 수 없습니다.");
             return null;
         }
+        console.log("자막 추출 성공 : " + track.getAttribute('src'));
 
-    };
+        // 비동기로 자막 내용을 가져와야함 ==> URL scheme must be "http" or "https" for CORS request. 에러 발생
+        // let test = await fetch(trackAddress)
+        //     .then(res => res.json())
+        //     .then(res => res);
 
-    xhr.open('GET', address, false); // 동기요청
-    xhr.send();
+        let subtitle = this.requestSutitle(trackAddress);
+        console.log(subtitle);
 
-    return subtitle;
-}
+        return subtitle;
+    }
+
+    requestSutitle(address) {
+        const xhr = new XMLHttpRequest();
+        let subtitle = '';
+
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+
+            if (xhr.status === 200) {
+                document.getElementById('content').innerHTML = xhr.responseText;
+                subtitle = xhr.responseText;
+            } else {
+                console.log(`[${xhr.status}] : ${xhr.statusText}`);
+                return null;
+            }
+
+        };
+
+        xhr.open('GET', address, false); // 동기요청
+        xhr.send();
+
+        return subtitle;
+    }
 
 }

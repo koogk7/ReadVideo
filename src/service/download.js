89f8981de884faa6182f171de676d549d6fcd23a
whiteun <maple1480@naver.com>
2019-10-28 21:44:30 +0900

    css 수정 & 다운로드 포맷 수정
    ###작업내용###
    1. 다운로드 파일 가독성 향상을 위해 줄바꿈 하나더 추가
    2. 다운로드 아이콘 삽입
    3. 버튼 아이콘 안보이는 css 수정

--- src/service/download.js
+++ src/service/download.js
@@ -4,17 +4,24 @@ export default class downloadAdmin{
     }
     
     downloadText(){
+        document.getElementById("download_img").setAttribute('src', "./image/download_on.png");
+
         let list = document.getElementsByClassName("subtitle_content");
         let text = "";
         for (let i = 0; i < list.length; i++) {
             text += list[i].textContent;
-            text += '\n';
+            text += '\n\n';
         }
         let data = new Blob([text], {type: 'text/plain'});
 
         let url = window.URL.createObjectURL(data);
 
         document.getElementById('downloadBtn').href = url;
+
+        setTimeout(function(){
+            document.getElementById("download_img").setAttribute('src', "./image/download_off.png");
+        },100);
+
     }
 
 }
